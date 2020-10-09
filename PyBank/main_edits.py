78c3a97a4ffc64d330b2b8767f622f7c9d750d2e
Samantha Ettinger <samanthaettinger@gmail.com>
2020-10-08 21:54:39 -0400

    Adds commentary to first few lines and removes unneccessary code

--- PyBank/main_edits.py
+++ PyBank/main_edits.py
@@ -1,57 +1,68 @@
+# Imports necessary packages
+# To read in csv
+
 import os
 import csv
-csvpath=os.path.join(os.getcwd(),'Python-Challenge_1', 'Resources','budget_data.csv')
+csvpath = os.path.join(os.getcwd(), 'Resources', 'budget_data.csv')
 print(csvpath)
+
+# Opens csvs using with
 
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-
-    # print(csvreader)
-
     csv_header = next(csvreader)
-    # print(f"CSV Header: {csv_header}")
 
-    start_row=0
-    row_list=[]
-    r_l=[]
-    sr=0
-    list_changes=[]
-    greatest_increase=[]
-    greatest_decrease=[]
+    start_row = 0
+    row_list = []
+    r_l = []
+    sr = 0
+    list_changes = []
+    greatest_increase = []
+    greatest_decrease = []
 
+# Skip header when looping through csv
 
-    skip_first_row=next(csvreader)
-    sr=sr+1
-    start_row=start_row+int(skip_first_row[1])
-    set_value=int(skip_first_row[1])
-   
-        
+    skip_first_row = next(csvreader)
     
-#gets number of months and net profits/losses
+
+# skip to the second row
+# converts the value in the second column to an int
+
+    start_row = start_row+int(skip_first_row[1])
+
+# skip to the first row
+# converts the value in the first column to an int
+
+    set_value = int(skip_first_row[1])
+
+# gets number of months and net profits/losses
+
     for row in csvreader:
-        if row[0]!=' ':
-            sr=sr+1
-            start_row=start_row+int(row[1])
-            value_changes=int(row[1])-set_value
-            set_value=int(row[1])
-            month_changes=row[0]
-            list_changes.append(value_changes)
-            list_changes.append(month_changes)
-            greatest_increase.append(value_changes)
-            grt=max(greatest_increase)
-            greatest_decrease.append(value_changes)
-            dec=min(greatest_decrease)
-            index_greatest_increase=list_changes.index(grt)
-            index_greatest_decrease=list_changes.index(dec)
-            total_average_profits_losses=sum(greatest_increase)/len(greatest_increase)
-        
-            
-       
-            
+        sr = sr+1
+# starting from the second row, counts the number of 
+# rows
+        start_row = start_row+int(row[1])
+# takes the value of the first row, converts it
+# to an int and subtracts it from the value in
+# the second row
+
+        value_changes = int(row[1])-set_value
+
+        # set_value = int(row[1])
+        month_changes = row[0]
+        list_changes.append(value_changes)
+        list_changes.append(month_changes)
+        greatest_increase.append(value_changes)
+        grt = max(greatest_increase)
+        greatest_decrease.append(value_changes)
+        dec = min(greatest_decrease)
+        index_greatest_increase = list_changes.index(grt)
+        index_greatest_decrease = list_changes.index(dec)
+        total_average_profits_losses = sum(greatest_increase)/len(greatest_increase)
     # r_l.append(sr)
     # row_list.append(start_row)
-    net_profits_losses=start_row
-    number_of_months=sr
+    net_profits_losses = start_row
+    number_of_months = sr
     print("Financial Analysis")
     print("------------------------")
     print("Total Months: " + str(number_of_months))
@@ -60,7 +71,6 @@ with open(csvpath, newline='') as csvfile:
     print("Greatest Increase in Profits: "+str(list_changes[index_greatest_increase+1])+" ($"+str(grt)+")")
     print("Greatest Decrease in Profits: "+str(list_changes[index_greatest_decrease+1])+" ($"+str(dec)+")")
 
-   
     output_file = os.path.join("budget_data_text.txt")
     with open(output_file, "w") as datafile:
         writer = csv.writer(datafile)
@@ -72,13 +82,3 @@ with open(csvpath, newline='') as csvfile:
         datafile.write("Average Change: -2315.12\n")
         datafile.write("Greatest Increase in Profits: Feb 2012 (1926159)\n")
         datafile.write("Greatest Decrease in Profits: Sep 2013 (-2196167)\n")
-
-   
-
-
-    
-
-    
-
-  
-            
